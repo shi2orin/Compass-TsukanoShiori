b31f8f7f22566fe1ec24e2b8a90c5c83a638b829
shi0rin <shiori1641276@gmail.com>
2023-10-11 17:58:57 +0900

    環境構築、リレーション

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,46 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
-    }
+        DB::table('users')->insert([
+                    [
+                        'over_name' => '田中',
+                        'under_name' => '一郎',
+                        'over_name_kana' => 'タナカ',
+                        'under_name_kana' => 'イチロウ',
+                        'mail_address' => 'tanakaichiro@example.com',
+                        'sex' => 1,
+                        'birth_day' => '2000-01-01',
+                        'role' => 4,
+                        'password' => bcrypt('password1'),
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ],
+                    [
+                        'over_name' => '田中',
+                        'under_name' => '次郎',
+                        'over_name_kana' => 'タナカ',
+                        'under_name_kana' => 'ジロウ',
+                        'mail_address' => 'tanakajiro@example.com',
+                        'sex' => 1,
+                        'birth_day' => '2000-01-01',
+                        'role' => 2,
+                        'password' => bcrypt('password2'),
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ],
+                    [
+                        'over_name' => '田中',
+                        'under_name' => '三郎',
+                        'over_name_kana' => 'タナカ',
+                        'under_name_kana' => 'サブロウ',
+                        'mail_address' => 'tanakasaburo@example.com',
+                        'sex' => 1,
+                        'birth_day' => '2000-01-01',
+                        'role' => 1,
+                        'password' => bcrypt('password3'),
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ],
+                ]);
+            }
 }
