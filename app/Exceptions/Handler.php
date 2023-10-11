feda634edb216ff5f68531aba6227db5c518753b
shi0rin <shiori1641276@gmail.com>
2023-10-11 22:31:45 +0900

    タイムアウトエラー記述追加

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,8 +48,12 @@ class Handler extends ExceptionHandler
      *
      * @throws \Exception
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
-    }
+		if ($exception instanceof TokenMismatchException) {
+			return redirect()->route('loginView');
+		}
+
+		return parent::render($request, $exception);
+	}
 }
